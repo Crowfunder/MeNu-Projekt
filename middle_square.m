f58d1a1752a8a8259ade5361da6d92d71391d1b6
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-01-03 14:18:39 +0100

    Update middle_square.m

--- middle_square.m
+++ middle_square.m
@@ -4,15 +4,15 @@ function s=middle_square(N,seed)
         square=seed^2;
         squareDigits=double(num2str(square)-double('0'));
         seedDigits=double(num2str(seed)-double('0'));
-        [sizeSquare,a]=size(squareDigits);
-        if(mod(sizeSquare,2)==1)
-            squareDigits=[0]+squareDigits;
+        [a, sizeSquare]=size(squareDigits);
+        while (length(squareDigits) ~= 8)
+            squareDigits=[0 squareDigits];
         end
       
         for j=3:6
             s(i)=s(i)+squareDigits(j)*10^(6-j);
         end
-        seed=s(i)
+        seed=s(i);
     end
 end
     
