f0e78a7a242854c6147b36ddb5043e810f8ed817
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-01-03 12:18:00 +0100

    Create randgens.m

---
+++ randgens.m
@@ -0,0 +1,21 @@
+function s=rand_mult(N,seed)
+    a=69069;
+    p=2^12;
+    s=zeros(N,1);
+    for i=1:N
+        s(i)=mod(seed*a,p);
+        seed=s(i);
+    end
+    s=s/p;
+end
+
+function s=rand_multiadd(N,seed)
+    a=69069;
+    p=2^32;
+    s=zeros(N,1);
+    for i=1:N
+        s(i)=mod(seed*a+m,p);
+        seed=s(i);
+    end
+    s=s/p;
+end
